a9d3cddbced189d5377a7e4d774a2b19c28d9cc1
svarogdoo <svarog.doo@gmail.com>
2024-07-21 18:46:49 +0200

    test api connection

--- domis.front/src/routes/+page.svelte
+++ domis.front/src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+  import { onMount } from "svelte";
+
+  let url = "https://localhost:7054/WeatherForecast";
+  let test = [];
+
+  onMount(async () => {
+    const res = await fetch(url);
+    test = await res.json();
+    console.info(test);
+  });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
