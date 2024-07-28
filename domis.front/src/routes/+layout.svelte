dc53b34334759697f35e1196ef7bf5d9be5fc34b
svarogdoo <svarog.doo@gmail.com>
2024-07-28 10:22:34 +0200

    add header

---
+++ domis.front/src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Header from "./Header.svelte";
+  import "./styles.css";
+</script>
+
+<div class="app">
+  <Header />
+
+  <main>
+    <slot />
+  </main>
+
+  <!-- <footer>
+      <p>© Copyright <a href="https://svarog.io/">svarog.io</a> 2024</p>
+    </footer> -->
+</div>
+
+<style>
+  .app {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+    background-color: white;
+    align-items: center;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    max-width: 64em;
+  }
+
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 12px;
+  }
+
+  footer a {
+    font-weight: bold;
+  }
+</style>
---
+++ domis.front/src/routes/Header.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  let searchTerm: string;
+</script>
+
+<header>
+  <nav>
+    <ul>
+      <li aria-current={$page.url.pathname === "/" ? "page" : undefined}>
+        <a href="/">domis enterijeri</a>
+      </li>
+      <input
+        type="text"
+        id="search-field"
+        placeholder="Enter Search Term"
+        autocomplete="off"
+        bind:value={searchTerm}
+        on:input
+      />
+      <li aria-current={$page.url.pathname === "/shop" ? "page" : undefined}>
+        <a href="/shop">ŠOP</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  header {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+  }
+
+  nav {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+  }
+
+  ul {
+    position: relative;
+    display: flex;
+    width: 100%;
+    margin-top: 2.3em;
+    padding: 0em 1em;
+    list-style: none;
+    justify-content: space-around;
+    align-items: center;
+    color: whitesmoke;
+    letter-spacing: 0.2em;
+    font-size: smaller;
+    max-width: 800px;
+  }
+
+  a:hover {
+    color: white;
+  }
+</style>
