982d3c671d19b71056833b9410cf8659fb5490b3
svarogdoo <svarog.doo@gmail.com>
2024-08-20 16:11:06 +0200

    category sidebar

--- domis.front/src/routes/[...slug]/+layout.svelte
+++ domis.front/src/routes/[...slug]/+layout.svelte
@@ -1,15 +1,11 @@
+<script>
+  import Sidebar from "./Sidebar.svelte";
+</script>
+
 <div class="product-search">
   <div class="category gap-x-12 pt-4">
-    <div class="product-sidebar gap-y-4">
-      <h2 class="text-xl">Svi proizvodi</h2>
-      <div class="sidebar-items rounded-md">
-        <p>Nedeljna akcija</p>
-        <p>Podovi</p>
-        <p>Dekorativne letvice</p>
-        <p>Nedeljna akcija</p>
-        <p>Podovi</p>
-        <p>Dekorativne letvice</p>
-      </div>
+    <div class="product-sidebar">
+      <Sidebar />
     </div>
     <div class="products mt-6">
       <slot />
@@ -24,22 +20,13 @@
     justify-content: center;
   }
 
+  .product-sidebar {
+    width: 17%;
+  }
+
   .category {
     width: 90%;
     display: flex;
-  }
-
-  .product-sidebar {
-    width: 15%;
-    display: flex;
-    flex-direction: column;
-  }
-  .sidebar-items {
-    display: flex;
-    flex-direction: column;
-    border-width: 1px;
-    border-color: #808080;
-    padding: 12px 26px;
   }
 
   .products {
