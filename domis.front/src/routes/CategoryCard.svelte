2b3315df07fac270ccc7dce5dbf9c400f4638ba7
svarogdoo <svarog.doo@gmail.com>
2024-08-23 18:55:59 +0200

    front page formatting

--- domis.front/src/routes/CategoryCard.svelte
+++ domis.front/src/routes/CategoryCard.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
   import backup from "$lib/assets/backup.jpg";
 
-  export let category = {
-    id: 1,
-    name: "",
-    image: "",
-  };
+  export let category: Category;
 </script>
 
-<a href="kategorija/83">
+<a href="kategorija/{category?.id}">
   <div class="w-full relative display flex justify-center items-center">
     <div class="absolute w-full h-96 bg-black"></div>
     <img
-      class="w-full h-96 object-cover opacity-80 text-white bg-slate-500 hover:opacity-60"
-      src={category.image}
-      alt={backup}
+      class="w-full h-96 object-cover opacity-60 text-white bg-slate-500 hover:opacity-60"
+      src={backup}
+      alt={category.name}
     />
-    <p class="absolute text-white tracking-widest text-xl">{category.name}</p>
+    <p
+      class="absolute text-white tracking-widest text-xl text-center font-medium"
+    >
+      {category.name}
+    </p>
   </div>
 </a>
