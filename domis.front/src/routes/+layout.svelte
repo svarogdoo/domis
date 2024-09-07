0819af83af93cab281d9a9ace74e5bd9f2cb116a
svarogdoo <svarog.doo@gmail.com>
2024-09-07 21:27:14 +0200

    hide sidebar on navigation change

--- domis.front/src/routes/+layout.svelte
+++ domis.front/src/routes/+layout.svelte
@@ -1,20 +1,32 @@
-<script>
+<script lang="ts">
   import Header from "./Header.svelte";
   import "./styles.css";
   import "../app.css";
   import Footer from "./Footer.svelte";
   import HeaderSidebar from "./HeaderSidebar.svelte";
+  import { page } from "$app/stores";
 
   let open = false;
+  let previousSlug: string;
+
+  $: if (previousSlug !== $page.url.pathname && open) {
+    // If not the first load, toggle the state when slug changes
+    if (previousSlug !== undefined) {
+      open = false;
+    }
+
+    // Update the previous slug
+    previousSlug = $page.url.pathname;
+  }
 </script>
 
 <div
-  class="relative w-full h-full bg-white flex flex-col justify-between items-center 2xl:w-3/4 2xl:m-auto"
+  class="relative w-full h-full bg-white flex flex-col items-center 2xl:w-3/4 2xl:m-auto"
 >
   <HeaderSidebar bind:open />
   <Header bind:sidebar={open} />
 
-  <main class="w-full flex flex-col pb-8">
+  <main class="w-full h-full flex flex-col pb-8">
     <slot />
   </main>
 
