05fb61df5ef4a4fbc7456f0d6a8bbe33c87a200a
svarogdoo <svarog.doo@gmail.com>
2024-10-19 14:22:59 +0200

    admin panel setup

---
+++ domis.front/src/routes/(admin)/admin/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import AdminPanelSidebar from "./AdminPanelSidebar.svelte";
+</script>
+
+<div class="flex w-full gap-x-4 justify-center">
+  <div class="hidden lg:flex max-h-48 mt-14 w-1/6 rounded-lg">
+    <AdminPanelSidebar />
+  </div>
+  <div class="flex w-full px-5 lg:px-0 lg:w-5/6 mt-14">
+    <slot />
+  </div>
+</div>
---
+++ domis.front/src/routes/(admin)/admin/AdminPanelSidebar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { userStore } from "../../../stores/user";
+
+  const handleLogout = () => {
+    userStore.logoutUser();
+  };
+</script>
+
+<ul class="flex flex-col w-full text-center text-lg">
+  <a
+    href="proizvodi"
+    class="w-full py-3 flex items-center justify-center hover:bg-gray-100 border-b border-t border-gray-600"
+  >
+    Proizvodi
+  </a>
+  <a
+    href="porudzbine"
+    class="w-full py-3 flex items-center justify-center hover:bg-gray-100 border-b border-gray-600"
+  >
+    Porudžbine
+  </a>
+  <a
+    href="korisnici"
+    class="w-full py-3 flex items-center justify-center hover:bg-gray-100 border-b border-gray-600"
+  >
+    Korisnici
+  </a>
+  <a
+    href="kategorije"
+    class="w-full py-3 flex items-center justify-center hover:bg-gray-100 border-b border-gray-600"
+  >
+    Kategorije
+  </a>
+  <li
+    class="w-full py-3 flex items-center justify-center hover:bg-gray-100 border-b border-gray-600"
+  >
+    <button on:click={handleLogout}> Izloguj se </button>
+  </li>
+</ul>
