d89ba0eea7baa228a466ab92887d3e45ee2d6e70
Luka Radovanovic <rluka996@gmail.com>
2024-10-19 14:41:28 +0200

    GET /user/orders

--- domis.api/DTOs/Order/UserOrderDto.cs
+++ domis.api/DTOs/Order/UserOrderDto.cs
@@ -5,6 +5,7 @@ public class UserOrderDto
     public int Id { get; set; }
     public int? StatusId { get; set; }
     public DateTime? Date { get; set; }
+    public string? Address { get; set; }
     public int? PaymentTypeId { get; set; }
     public int? PaymentStatusId { get; set; }
     public decimal? PaymentAmount { get; set; }
