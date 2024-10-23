fcadb2f3bcc6a74580e0fcff891f6359ebb1973f
Luka Radovanovic <rluka996@gmail.com>
2024-10-23 12:04:03 +0200

    BE: email confirmation beautify FE: introduce pagination for products

--- domis.front/src/routes/(products)/kategorija/[id]/+page.svelte
+++ domis.front/src/routes/(products)/kategorija/[id]/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { page } from "$app/stores";
   import ProductCard from "./CategoryProductCard.svelte";
   import { getCategoryProducts } from "../../../../services/category-service";
-  import { getLastSlug } from "../../../../helpers/slugParsing";
+  import { onMount } from "svelte";
 
   export let data;
 
@@ -11,6 +9,9 @@
   let products: Array<CategoryProduct> = [];
   let isOpen = false;
   let sortType = "low-to-high";
+
+  let pageNumber = 1; // Default page
+  let pageSize = 18;  // Default page size
 
   $: products = data.props.products;
   $: categoryDetails = data.props.category;
@@ -35,6 +36,29 @@
     sortType = value;
     isOpen = false;
   }
+  
+  async function fetchProducts() {
+    const categoryData = await getCategoryProducts(Number.parseInt(categoryDetails.id), pageNumber, pageSize);
+    products = categoryData.products;
+  }
+
+  function changePage(delta: number) {
+    pageNumber += delta;
+    fetchProducts();  // Fetch new data based on the updated page number
+
+    const newUrl = new URL(window.location.href);
+    newUrl.searchParams.set("stranica", pageNumber.toString());
+    history.pushState({}, '', newUrl);
+  }
+
+  onMount(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const pageParam = urlParams.get("stranica");
+    if (pageParam) {
+      pageNumber = Number(pageParam);
+    }
+    fetchProducts(); // Fetch products for the current page
+  });
 </script>
 
 <section class="w-full flex flex-col justify-center">
@@ -44,32 +68,19 @@
     </h2>
     <div class="relative flex">
       <div class="text-sm lg:text-normal">
-        <button
-          on:click={toggleDropdown}
-          class="flex gap-x-2 font-light items-center"
-          >Sortiraj prema:
-
-          <p
-            class="ring-1 rounded-lg ring-gray-500 px-2 lg:px-4 py-1 lg:py-2 font-light"
-          >
+        <button on:click={toggleDropdown} class="flex gap-x-2 font-light items-center">
+          Sortiraj prema:
+          <p class="ring-1 rounded-lg ring-gray-500 px-2 lg:px-4 py-1 lg:py-2 font-light">
             {sortType === "low-to-high" ? "Cena rastuće" : "Cena opadajuće"}
-          </p></button
-        >
+          </p>
+        </button>
       </div>
       {#if isOpen}
-        <ul
-          class="absolute z-10 right-0 mt-12 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-        >
-          <button
-            on:click={() => setSortType("low-to-high")}
-            class="w-full text-right py-3 px-3 hover:bg-gray-700 hover:text-white rounded-lg"
-          >
+        <ul class="absolute z-10 right-0 mt-12 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <button on:click={() => setSortType("low-to-high")} class="w-full text-right py-3 px-3 hover:bg-gray-700 hover:text-white rounded-lg">
             Cena rastuće
           </button>
-          <button
-            on:click={() => setSortType("high-to-low")}
-            class="w-full text-right py-3 px-3 hover:bg-gray-700 hover:text-white rounded-lg"
-          >
+          <button on:click={() => setSortType("high-to-low")} class="w-full text-right py-3 px-3 hover:bg-gray-700 hover:text-white rounded-lg">
             Cena opadajuće
           </button>
         </ul>
@@ -81,4 +92,25 @@
       <ProductCard {product} />
     {/each}
   </div>
+
+  <!-- Pagination Controls -->
+<div class="flex justify-end mt-4">
+  <div class="flex space-x-2">
+    <button 
+      on:click={() => changePage(-1)} 
+      disabled={pageNumber === 1} 
+      class="disabled:opacity-50"
+    >
+      Prethodna
+    </button>
+    <button 
+      on:click={() => changePage(1)} 
+      disabled={products.length < pageSize}
+      class="disabled:opacity-50"
+    >
+      Sledeća
+    </button>
+  </div>
+</div>
+
 </section>
--- domis.front/src/routes/(products)/kategorija/[id]/+page.ts
+++ domis.front/src/routes/(products)/kategorija/[id]/+page.ts
@@ -1,9 +1,11 @@
 import { getCategoryProducts } from "../../../../services/category-service.js";
 
-export async function load({ params }) {
+export async function load({ params, url }) {
   const categoryId = params.id;
+  const pageNumber = url.searchParams.get("page") || "1";
+  const pageSize = url.searchParams.get("size") || "18";
 
-  let categoryData = await getCategoryProducts(Number.parseInt(categoryId));
+  let categoryData = await getCategoryProducts(Number.parseInt(categoryId), Number.parseInt(pageNumber), Number.parseInt(pageSize));
 
   return {
     props: {
