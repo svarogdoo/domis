c3150609d65663ff5b016bce85d589f010998c8a
Luka Radovanovic <rluka996@gmail.com>
2024-11-01 16:18:18 +0100

    FE: categories -> show 2 in row on mobile

--- domis.front/src/routes/+page.svelte
+++ domis.front/src/routes/+page.svelte
@@ -10,7 +10,7 @@
 
 <section class="mt-4 w-full flex justify-center">
   <div
-    class="w-11/12 xl:w-3/4 category-cards grid grid-cols-1 lg:grid-cols-3 gap-x-4 gap-y-4"
+    class="w-11/12 xl:w-3/4 category-cards grid grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-4"
   >
     {#each categoriesList as category}
       <CategoryCard {category} />
