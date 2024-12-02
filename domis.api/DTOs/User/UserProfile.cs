c8e9e4de8fc83454d487b31c5e5fec206dbb2a46
Luka Radovanovic <rluka996@gmail.com>
2024-12-02 16:02:04 +0100

    Update UserProfile.cs, UserProfileDto.cs, and AddressRepository.cs

--- domis.api/DTOs/User/UserProfile.cs
+++ domis.api/DTOs/User/UserProfile.cs
@@ -2,6 +2,16 @@
 
 namespace domis.api.DTOs.User;
 
+public record UserProfileDto(
+    string FirstName, 
+    string LastName,
+    string Email, 
+    string? PhoneNumber, 
+    CompanyInfo? CompanyInfo, 
+    AddressEntity? AddressDelivery, 
+    AddressEntity? AddressInvoice
+);
+
 public interface IUserProfileDto
 {
     string FirstName { get; }
@@ -9,16 +19,18 @@ public interface IUserProfileDto
     string Email { get; }
     string? PhoneNumber { get; }
     CompanyInfo? CompanyInfo { get; }
+    AddressEntity? AddressDelivery { get; }
+    AddressEntity? AddressInvoice { get; }
 }
 
-public record UserProfileDto(
+public record RegularUserProfileDto(
     string FirstName, string LastName,
-    string Email, string? PhoneNumber, CompanyInfo? CompanyInfo
+    string Email, string? PhoneNumber, CompanyInfo? CompanyInfo, AddressEntity? AddressDelivery, AddressEntity? AddressInvoice
 ) : IUserProfileDto;
 
 public record UserWholesaleProfileDto(
     string FirstName, string LastName,
-    string Email, string? PhoneNumber, CompanyInfo? CompanyInfo
+    string Email, string? PhoneNumber, CompanyInfo? CompanyInfo, AddressEntity? AddressDelivery, AddressEntity? AddressInvoice
 ) : IUserProfileDto;
 
 
--- domis.api/Repositories/AddressRepository.cs
+++ domis.api/Repositories/AddressRepository.cs
@@ -1,11 +1,17 @@
+using domis.api.Models;
+
 namespace domis.api.Repositories;
 
 public interface IAddressRepository
 {
-    
+    Task<IEnumerable<AddressEntity>> GetUserAddressesAsync(string userId);
+    Task<AddressEntity?> GetAddressByIdAsync(int addressId);
+    Task<bool> UpdateAddressAsync(AddressEntity address);
+    Task<int> AddAddressAsync(AddressEntity address);
+    Task<bool> DeleteAddressAsync(int addressId);
 }
 
-public class AddressRepository : IAddressRepository
+public class AddressRepository
 {
     
 }
